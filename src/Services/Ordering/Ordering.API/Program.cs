48cf838e146599fb296f78498d276be8c2647e5e
Ekaterina Demidova <e.demidova.by@gmail.com>
2024-06-26 00:09:35 +0300

    feat: add ordering microservices with DDD, CQRS and Clean Architecture

---
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -0,0 +1,17 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastucture;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services
+    .AddApplicationServices()
+    .AddInfrastuctureServices(builder.Configuration)
+    .AddApiServices();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.Run();
