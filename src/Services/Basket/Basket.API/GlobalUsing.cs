3b1fbfe2bca17e1289a554818ed9a7ba23cf4428
Ekaterina Demidova <e.demidova.by@gmail.com>
2024-06-25 21:26:02 +0300

    feat: add feat: add discount.grpc microservices for microservices grpc communication

--- src/Services/Basket/Basket.API/GlobalUsing.cs
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -4,6 +4,7 @@ global using Basket.API.Data;
 global using BuildingBlocks.CQRS;
 global using BuildingBlocks.Behaviors;
 global using BuildingBlocks.Exceptions;
+global using BuildingBlocks.Exceptions.Handler;
 global using Carter;
 global using Mapster;
 global using MediatR;
