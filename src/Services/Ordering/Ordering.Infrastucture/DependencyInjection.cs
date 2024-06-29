8c334cefb7d7aaf714dac4918fe6899d3d06f859
Ekaterina Demidova <e.demidova.by@gmail.com>
2024-06-30 01:11:45 +0300

    feat: develop ordering.application layer with CQRS and MediatR

--- src/Services/Ordering/Ordering.Infrastucture/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastucture/DependencyInjection.cs
@@ -1,10 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Ordering.Application.Data;
 
 namespace Ordering.Infrastucture
 {
@@ -15,11 +12,18 @@ namespace Ordering.Infrastucture
         {
             var connectionString = configuration.GetConnectionString("Database");
 
-            //// Add services to the container.
-            //services.AddDbContext<ApplicationDbContext>(options => 
-            //    options.UseSqlServer(connectionString));
+            // Add services to the container.
+            services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+            services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
 
-            //services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+            services.AddDbContext<ApplicationDbContext>((sp, options) =>
+            {
+                options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+                options.UseSqlServer(connectionString);
+            });
+                
+
+            services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
 
             return services;
         }
